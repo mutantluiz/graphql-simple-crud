60475b2c0b8e40e1fcb66711e42445daf3bb5eb5
luiz filho <luiz.filho@mutantbr.com>
2019-09-01 14:32:02 -0300

    Delete Update Create

--- index.js
+++ index.js
@@ -11,26 +11,57 @@ const typeDefs = gql`
     type Query {
         quotes: [Quote]
     }
+
+    type Mutation {
+        addQuote(phrase: String!, quotee: String): Quote
+        editQuote(id: ID!, phrase: String, quotee: String): Quote
+        deleteQuote(id: ID!): DeleteResponse
+    }
+
+    type DeleteResponse {
+        ok: Boolean!
+    }
 `
 
 const quotes = {}
-const addQuotes = quote => {
+const addQuote = quote => {
     const id = uuid()
     return quotes[id] = { ...quote, id }
 }
 
-addQuotes({ phrase: "X", quotee: "x"})
-addQuotes({ phrase: "Y", quotee: "y"})
-addQuotes({ phrase: "W", quotee: "w"})
+addQuote({ phrase: "LH", quotee: "44"})
+addQuote({ phrase: "VB", quotee: "77"})
+addQuote({ phrase: "MV", quotee: "33"})
 
 const resolvers = {
     Query: {
         quotes: () => Object.values(quotes)
-    }
+    },
+    Mutation: {
+        addQuote: async (parent, quote) => {
+            return addQuote(quote)
+        },
+        editQuote: async (parent, {id, ...quote}) => {
+            if (!quotes[id]) {
+                throw new Error("Quote doesn't exist")
+            }
+
+            quotes[id] = {
+                ...quotes[id],
+                ...quote,
+            }
+
+            return quotes[id]
+        },
+        deleteQuote: async (parent, { id }) => {
+            const ok = Boolean(quotes[id])
+            delete quotes[id]
+
+            return { ok }
+        },
+    },
 }
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
-server.listen().then( ({ url }) => {
-    console.log(`🚀 Server ready at ${url}`)
-} )
+server.listen()
